0eff2423a2c85b049d192ed1d32c5989774fe9f1
Prasad Chathuranga <surenbandara7@gmail.com>
2023-10-27 10:06:13 +0530

    Revert "Test"
    This reverts commit e45b0b915c981ac38eaf41ff01d94f3150e25c1e.

--- after_process.py
+++ after_process.py
@@ -6,10 +6,10 @@ import matplotlib.image as mpimg
 import pytesseract
 from pytesseract import Output
 from PIL import Image
-from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from transformers import TrOCRProcessor, TFVisionEncoderDecoderModel
 
 processor = TrOCRProcessor.from_pretrained("./microsoft/trocr-base-handwritten")
-model = VisionEncoderDecoderModel.from_pretrained('./microsoft/trocr-base-handwritten',from_pt=True)
+model = TFVisionEncoderDecoderModel.from_pretrained('./microsoft/trocr-base-handwritten',from_pt=True)
 
 
 name = 'image0.jpg'
